c57d76a20bd7bab98a1bfc8c6b68921e458ae31b
FerBarahona1 <fsbetancourth@unah.hn>
2024-10-14 22:02:36 -0600

    Editando unos detallitos

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -19,47 +19,47 @@ class PermissionSeeder extends Seeder
 
         // //Permisos del CRUD PAIS
         Permission::create(['name' => 'Ver Paises']);
-        // Permission::create(['name' => 'crear-pais']);
-        // Permission::create(['name' => 'editar-pais']);
-        // Permission::create(['name' => 'eliminar-pais']);
+        Permission::create(['name' => 'crear-pais']);
+        Permission::create(['name' => 'editar-pais']);
+        Permission::create(['name' => 'eliminar-pais']);
 
         // //Permisos del CRUD DEPARTAMENTO
-         Permission::create(['name' => 'Ver Departamento']);
-        // Permission::create(['name' => 'crear-departamento']);
-        // Permission::create(['name' => 'editar-departamento']);
-        // Permission::create(['name' => 'eliminar-departamento']);
+        Permission::create(['name' => 'Ver Departamento']);
+        Permission::create(['name' => 'crear-departamento']);
+        Permission::create(['name' => 'editar-departamento']);
+        Permission::create(['name' => 'eliminar-departamento']);
 
         // //Permisos del CRUD MUNICIPIO
         Permission::create(['name' => 'Ver Municipios']);
-        // Permission::create(['name' => 'crear-municipio']);
-        // Permission::create(['name' => 'editar-municipio']);
-        // Permission::create(['name' => 'eliminar-municipio']);
+        Permission::create(['name' => 'crear-municipio']);
+        Permission::create(['name' => 'editar-municipio']);
+        Permission::create(['name' => 'eliminar-municipio']);
 
         //Permisos del CRUD CIUDAD
         Permission::create(['name' => 'Ver Ciudades']);
-        // Permission::create(['name' => 'crear-ciudad']);
-        // Permission::create(['name' => 'editar-ciudad']);
-        // Permission::create(['name' => 'eliminar-ciudad']);
+        Permission::create(['name' => 'crear-ciudad']);
+        Permission::create(['name' => 'editar-ciudad']);
+        Permission::create(['name' => 'eliminar-ciudad']);
 
         //Permisos del CRUD ROLES
          Permission::create(['name' => 'Ver Roles']);
-        // Permission::create(['name' => 'crear-rol']);
-        // Permission::create(['name' => 'editar-rol']);
-        // Permission::create(['name' => 'eliminar-rol']);
+        Permission::create(['name' => 'crear-rol']);
+        Permission::create(['name' => 'editar-rol']);
+        Permission::create(['name' => 'eliminar-rol']);
 
         //Permisos del CRUD PERMISOS
          Permission::create(['name' => 'Ver Permisos']);
 
          //Permisos del CRUD ROLES
         Permission::create(['name' => 'Ver Usuarios']);
-        // Permission::create(['name' => 'crear-usuario']);
-        // Permission::create(['name' => 'editar-usuario']);
-        // Permission::create(['name' => 'eliminar-usuario']);
+        Permission::create(['name' => 'crear-usuario']);
+        Permission::create(['name' => 'editar-usuario']);
+        Permission::create(['name' => 'eliminar-usuario']);
 
         //Permisos del CRUD EMPLEADOS
         Permission::create(['name' => 'Ver Empleados']);
-        // Permission::create(['name' => 'crear-empleado']);
-        // Permission::create(['name' => 'editar-empleado']);
-        // Permission::create(['name' => 'eliminar-empleado']);
+        Permission::create(['name' => 'crear-empleado']);
+        Permission::create(['name' => 'editar-empleado']);
+        Permission::create(['name' => 'eliminar-empleado']);
     }
 }
