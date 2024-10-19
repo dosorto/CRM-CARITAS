91a67491a49ba8ed2221e37d7c072e7c41300b13
FerBarahona1 <fsbetancourth@unah.hn>
2024-10-19 16:50:48 -0600

    Agregando migraciones de Articulos, categoria y subcategoria

--- app/Models/Articulo.php
+++ app/Models/Articulo.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Articulo extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'articulos';
+
+    public function subcategoria()
+    {
+        return $this->belongsTo(SubCategoria::class); // Relación con subcategoría
+    }
+
+
 }
---
+++ app/Models/SubCategoria.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class SubCategoria extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+
+    protected $table = 'subcategorias';
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class); // Relación con categorías
+    }
+
+    public function articulos(): HasMany
+    {
+        return $this->hasMany(Articulo::class); // Relación con artículos
+    }
+
+
+}
