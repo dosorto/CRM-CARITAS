0d74d654a348fa7986a1eb171a00da09791a859c
Marioo83GitHub <mcarbajalg@unah.hn>
2024-10-26 04:14:30 -0600

    modelo de Articulo

---
+++ app/Models/Articulo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Articulo extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = "articulos";
+
+    public function subcategorias(): BelongsTo
+    {
+        return $this->belongsTo(SubCategoria::class);
+    }
+}
