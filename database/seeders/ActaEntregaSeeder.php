ca28515bb9671b947982d6f4766a8884c0605bf4
Marioo83GitHub <mcarbajalg@unah.hn>
2024-11-04 07:12:52 -0600

    modificado seeder de acta

--- database/seeders/ActaEntregaSeeder.php
+++ database/seeders/ActaEntregaSeeder.php
@@ -12,28 +12,28 @@ class ActaEntregaSeeder extends Seeder
      */
     public function run(): void
     {
-        Articulo::create([
-            'nombre' => 'Cable Impresora',
-            'descripcion' => 'cable de impresora de 2 metros',
-            'codigo_barra' => '6957303828470',
-            'cantidad_stock' => 10,
-            'subcategoria_id' => 1,
-        ]);
+        // Articulo::create([
+        //     'nombre' => 'Cable Impresora',
+        //     'descripcion' => 'cable de impresora de 2 metros',
+        //     'codigo_barra' => '6957303828470',
+        //     'cantidad_stock' => 10,
+        //     'subcategoria_id' => 1,
+        // ]);
 
-        Articulo::create([
-            'nombre' => 'Kellogs Corn Flakes',
-            'descripcion' => 'Corn Flakes de 150g',
-            'codigo_barra' => '7501008101049',
-            'cantidad_stock' => 30,
-            'subcategoria_id' => 1,
-        ]);
+        // Articulo::create([
+        //     'nombre' => 'Kellogs Corn Flakes',
+        //     'descripcion' => 'Corn Flakes de 150g',
+        //     'codigo_barra' => '7501008101049',
+        //     'cantidad_stock' => 30,
+        //     'subcategoria_id' => 1,
+        // ]);
 
-        Articulo::create([
-            'nombre' => 'Resma de Papel Carta',
-            'descripcion' => 'Premium Laser Paper',
-            'codigo_barra' => '7891191002460',
-            'cantidad_stock' => 5,
-            'subcategoria_id' => 1,
-        ]);
+        // Articulo::create([
+        //     'nombre' => 'Resma de Papel Carta',
+        //     'descripcion' => 'Premium Laser Paper',
+        //     'codigo_barra' => '7891191002460',
+        //     'cantidad_stock' => 5,
+        //     'subcategoria_id' => 1,
+        // ]);
     }
 }
