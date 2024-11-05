67584b4f924832b47483fbe10366c3bfa16b6566
Ingrid <baquedanoxingridx@gmail.com>
2024-11-04 22:33:41 -0600

    librerias agregadas, dashboard funcional y reportes mensuales fucnionales

--- resources/views/livewire/reportes/reporte-mensual.blade.php
+++ resources/views/livewire/reportes/reporte-mensual.blade.php
@@ -9,16 +9,18 @@
             <input wire:model.live="fecha_final" type="date" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
         </div>
 </div>
-<p>
-    {{ $fecha_inicio  }}
-   {{ $fecha_final}}
-</p>
+<div class="w-full flex justify-end">
+    <button onclick="window.print()" type="button" class="text-white bg-[#ad342b] hover:bg-[#ad342b]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 me-2 mb-2">
+        Imprimir Reporte
+        </button>
+</div>
 <div class=" flex justify-center w-full p-4">
 
-    <div style="width: 8.5in; height: 11in; background-color: #fffcf6; font-family: Arial, Helvetica, sans-serif;">
+    <div class="print-section" style="width: 8.5in; height: 11in; background-color: #fffcf6; font-family: Arial, Helvetica, sans-serif;">
         <!-- Logos -->
      <div class="flex w-full justify-center m-2" >
-        <img class="h-16" src="{{ asset('imagenes/reportes/Recurso 1.png') }}" alt="">
+        <img class="h-16" src="{{ asset('img/LOGO1.png') }}" alt="">
+        <img class="h-16" src="{{ asset('img/LOGO2.png') }}" alt="">
      </div>
      <!-- Ubicacion -->
      <div class="flex justify-center mb-2">
@@ -32,7 +34,7 @@
                 </p>
             </p>
             <span style="color: #583d2b; font-weight: 700; font-family: Arial, Helvetica, sans-serif;">
-                Septiembre 24
+                {{ $nombreMes }} {{ $numeroDia }}
 
             </span>
          </div>
@@ -40,7 +42,7 @@
 
    <!-- Recuadro rojo -->
         <div class="flex items-center justify-center w-full">
-        <div style="width: 80%; background-color: #ad342b; border-color: #8a2e2e; color: white; " class="border flex items-center p-2">
+        <div style="width: 80%; border-color: #8a2e2e; color: white; " class="fondo border flex items-center p-2">
             <img style="filter: contrast(150%) brightness(90%) drop-shadow(0 0 5px rgba(0, 0, 0, 0.8));" class="h-14" src="{{ asset('imagenes/reportes/icon-corazon.png') }}" alt="">
             <div class="text-center" style="font-size: 14px;">
                 <p  >
@@ -61,7 +63,7 @@
                     <img class="h-28" src="{{ asset('imagenes/reportes/family.png') }}" alt="">
                 </div>
                 <p style="color: #583d2b; font-size: 24px;" class="flex justify-center font-extrabold">
-                    35
+                    {{ $cantidad_migrantes }}
                 </p>
                 <div style="color: #583d2b;" class="flex justify-center font-semibold text-center">
                     personas migrantes y refugiadas
@@ -72,7 +74,7 @@
             <div class="col-span-2  grid grid-cols-4 md:grid-cols-4 gap-4 items-end pb-5">
                 <div style="display: flex; align-items: end;"  class="flex flex-col items-end">
                     <div class="text-center w-full font-extrabold" style="font-size: 20px; color: #583d2b;">
-                        16
+                        {{ $cantidad_hombres }}
                     </div>
                     <div class="text-center w-full mb-2" style="color:#583d2b;">
                         hombres
@@ -83,7 +85,8 @@
                 </div>
                 <div>
                     <div class="text-center w-full font-extrabold" style="font-size: 20px; color: #583d2b;">
-                        10
+                        {{ $cantidad_mujeres }}
+
                     </div>
                     <div class="text-center w-full mb-2" style="color:#583d2b;">
                         mujeres
@@ -94,7 +97,7 @@
                 </div>
                 <div>
                     <div class="text-center w-full font-extrabold" style="font-size: 20px; color: #583d2b;">
-                        5
+                        {{ $cantidad_ninos }}
                     </div>
                     <div class="text-center w-full mb-2" style="color:#583d2b;">
                         niños
@@ -105,7 +108,8 @@
                 </div>
                 <div>
                     <div class="text-center w-full font-extrabold" style="font-size: 20px; color: #583d2b;">
-                        4
+                        {{ $cantidad_ninas }}
+
                     </div>
                     <div class="text-center w-full mb-2" style="color:#583d2b;">
                         niñas
@@ -131,12 +135,16 @@
             Nacionalidad
         </p>
         <div class="grid grid-cols-2 md:grid-cols-2 gap-4">
+
             <div class="flex justify-end">
-                <!-- Aqui iria el grafico -->
-                <img src="" alt="" class="h-72">
+                @if ($grafico)
+                {{-- <x-chartjs-component :chart="$chart" /> --}}
+                {!! $this->chart->render() !!}
+                @endif
             </div>
+
             <div class="grid justify-start items-end" style="padding-bottom: 65px;">
-                <div style="font-size: 16px; color: #583d2b;" class="text-center  w-96">
+                <div style="font-size: 16px; color: #583d2b;" class="text-center w-96">
                     <p>Durante el mes</p>
                     <span class="font-bold" style="font-size: 17;"> 18 </span> personas fueron identificadas como
                     <p style="font-size: 17px;" class="font-bold">Migrantes en tránsito</p>
@@ -151,12 +159,12 @@
         <div class="flex flex-col text-center items-end p-3">
             <div class="flex flex-col items-center">
                 <div class="flex items-center justify-center" style="font-size: 30px; font-weight: 600; background-color: #ad342b; border-radius: 50%; width: 70px; height: 70px; color: #fffcf6;">
-                    462
+                    {{ $total_personas }}
                 </div>
                 <div class="font-bold mt-2" style="color: #583d2b; font-size: 18px;">
                     <p>total de personas</p>
                     <p>en condición de movilidad</p>
-                    <p>atendidas en 2024</p>
+                    <p>atendidas en {{ $currentYear }}</p>
                 </div>
             </div>
 
@@ -176,6 +184,31 @@
   </div>
 
 
+<style>
+    .fondo {
+        background-color: #ad342b !important;
+    }
+  @media print {
+    /* Ocultar todo lo que no esté en la sección de impresión */
+    body * {
+        visibility: hidden;
+    }
+
+    /* Solo mostrar el contenido de print-section */
+    .print-section, .print-section * {
+        visibility: visible;
+    }
+
+    /* Asegurar que print-section ocupe toda la página impresa */
+    .print-section {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+    }
+
+
+}
+</style>
+
 </div>
-
-
