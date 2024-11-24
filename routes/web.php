4278c318527b427167a623c22aadc8560c58b55b
Ingrid <baquedanoxingridx@gmail.com>
2024-11-23 23:26:18 -0600

    recobrando cambios

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ use App\Livewire\Crud\Discapacidades\VerDiscapacidades;
 use App\Livewire\Crud\Fronteras\VerFronteras;
 use App\Livewire\Crud\SituacionesMigratorias\VerSituacionesMigratorias;
 use App\Livewire\Crud\TipoDonantes\VerTipoDonantes;
+use App\Livewire\Reportes\ReporteArticulo;
 
 Route::get('/inicio', Dashboard::class)
     ->middleware('auth');
@@ -139,4 +140,8 @@ Route::get('/asesores-migratorios', VerAsesoresMigratorios::class)
 
 Route::get('/fronteras', VerFronteras::class)
     ->name('ver-fronteras')
+    ->middleware('auth');
+
+ Route::get('/reporte-articulos', ReporteArticulo::class)
+    ->name('reporte-articulos')
     ->middleware('auth');
