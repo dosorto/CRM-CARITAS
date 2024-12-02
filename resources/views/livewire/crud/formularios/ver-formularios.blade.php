36db53f18c54009baf9d1a6bc74575eef720cc10
Marioo83GitHub <mcarbajalg@unah.hn>
2024-12-02 07:22:44 -0600

    cambiada imagen de exodo por la del centro en documentos para imprimir

--- resources/views/livewire/crud/formularios/ver-formularios.blade.php
+++ resources/views/livewire/crud/formularios/ver-formularios.blade.php
@@ -4,7 +4,7 @@
         class="relative bg-white p-8 rounded-lg shadow-lg max-w-3xl w-full space-y-6 print-section">
 
         <!-- Logo superior izquierdo -->
-        <img src="/img/LOGO1.png" class="absolute top-4 left-0 h-10 w-30 m-4" alt="Logo izquierdo">
+        <img src="/img/logo-centro.jpg" class="absolute top-4 left-6 h-16 w-16 m-4" alt="Logo izquierdo">
 
         <!-- Logo superior derecho -->
         <img src="/img/LOGO2.png" class="absolute top-3 right-0 h-10 w-30 m-4" alt="Logo derecho">
