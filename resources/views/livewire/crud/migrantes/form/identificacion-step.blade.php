e26988853aba9ef85308a49317a7de11e35d2eb1
Marioo83GitHub <mcarbajalg@unah.hn>
2024-12-30 03:52:40 -0600

    agregado machote de paso de identificacion

--- resources/views/livewire/crud/migrantes/form/identificacion-step.blade.php
+++ resources/views/livewire/crud/migrantes/form/identificacion-step.blade.php
@@ -1,26 +1,7 @@
-<main class="flex-grow overflow-hidden flex flex-col">
-
-    <section class="flex justify-between my-4 items-center">
-        <h2 class="text-lg">
-            <strong>Paso 1:</strong> Identificación de la Persona
-        </h2>
-        <livewire:components.forms.steps :currentStep="1" :steps="4">
-    </section>
-
-    <article class="flex-grow overflow-y-auto rounded-lg border-2 border-accent p-6">
-
-        <div class="flex flex-col">
-            <strong>Ingrese un número de Identificación (Pasaporte o DNI)</strong>
-            <input wire:model="identificacion" class="input mt-2 bg-accent" type="text" placeholder="Ej. 0601200303381">
-            @error('identificacion')
-                <p class="text-error font-semibold">{{ $message }}</p>
-            @enderror
-        </div>
-
-    </article>
-
-    <footer class="py-4 flex gap-4 justify-between">
-        <livewire:crud.migrantes.listado-migrantes-button />
-        <livewire:components.buttons.next-step-button />
-    </footer>
-</main>
+<div class="flex flex-col">
+    <strong>Ingrese un número de Identificación (Pasaporte o DNI)</strong>
+    <input wire:model="identificacion" class="input mt-2 bg-accent" type="text" placeholder="Ej. 0601200303381">
+    @error('identificacion')
+        <p class="text-error font-semibold">{{ $message }}</p>
+    @enderror
+</div>
--- resources/views/livewire/crud/migrantes/registrar-migrante.blade.php
+++ resources/views/livewire/crud/migrantes/registrar-migrante.blade.php
@@ -11,66 +11,13 @@
         </div>
     </header>
 
-
-    {{-- <div class="size-full flex flex-col h-full my-4">
-        <div role="tablist" class="tabs tabs-lifted"> <!-- tabs normal sin flex -->
-            <input type="radio" name="steps" role="tab" class="tab font-semibold pointer-events-none"
-                aria-label="Paso 1 @if ($currentStep == 1) : {{ $stepNames[1] }} @endif"
-                wire:model="currentStep" value="1" />
-            <input type="radio" name="steps" role="tab" class="tab font-semibold pointer-events-none"
-                aria-label="Paso 2 @if ($currentStep == 2) : {{ $stepNames[2] }} @endif"
-                wire:model="currentStep" value="2" />
-            <input type="radio" name="steps" role="tab" class="tab font-semibold pointer-events-none"
-                aria-label="Paso 3 @if ($currentStep == 3) : {{ $stepNames[3] }} @endif"
-                wire:model="currentStep" value="3" />
-            <input type="radio" name="steps" role="tab" class="tab font-semibold pointer-events-none"
-                aria-label="Paso 4 @if ($currentStep == 4) : {{ $stepNames[4] }} @endif"
-                wire:model="currentStep" value="4" />
-            <input type="radio" name="steps" role="tab" class="tab font-semibold pointer-events-none"
-                aria-label="Paso 5 @if ($currentStep == 5) : {{ $stepNames[5] }} @endif"
-                wire:model="currentStep" value="5" />
-        </div>
-
-        <!-- Contenedor separado para los paneles -->
-        <div class="flex-grow flex flex-col h-full border-4 border-base-100">
-            <!-- Contenedor de paneles con flex y altura -->
-            @if ($currentStep == 1)
-                <div class="p-4">
-                    Identificación...
-                </div>
-            @endif
-
-            @if ($currentStep == 2)
-                <div class="p-4">
-                    Datos Personales...
-                </div>
-            @endif
-            @if ($currentStep == 3)
-                <div class="p-4">
-                    Registro Familiar...
-                </div>
-            @endif
-            @if ($currentStep == 4)
-                <div class="p-4">
-                    Situación Migratoria...
-                </div>
-            @endif
-            @if ($currentStep == 5)
-                <div class="p-4">
-            Necesidades y Observaciones...
-                </div>
-            @endif
-        </div>
-    </div> --}}
-
-
     <div class="size-full flex-grow my-4 border-x-4 rounded-xl border-b-4 border-base-300">
         <div role="tablist" class="tabs tabs-lifted">
             <input type="radio" name="steps" role="tab" class="tab font-semibold pointer-events-none"
                 aria-label="Paso 1 @if ($currentStep == 1) : {{ $stepNames[1] }} @endif"
                 wire:model="currentStep" value="1" />
             <div role="tabpanel" class="h-full flex-grow tab-content bg-neutral  rounded-box p-6">
-                Identificación...
+                <livewire:crud.migrantes.form.identificacion-step />
             </div>
 
             <input type="radio" name="steps" role="tab" class="tab font-semibold pointer-events-none"
