7e8853fe507e6c0a935ada651b370801fbbe7ac3
Marioo83GitHub <mcarbajalg@unah.hn>
2025-04-11 07:15:02 -0600

    merge de DevMario para lo de reporteria y encuestas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,27 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-
-            // Dev Seeders
-            // -----------------------------------------
-            PaisSeeder::class,
-            DepartamentoSeeder::class,
-            CiudadSeeder::class,
-            DiscapacidadSeeder::class,
-            SituacionMigratoriaSeeder::class,
-            AsesorMigratorioSeeder::class,
-            FronteraSeeder::class,
-            MotivoSalidaPaisSeeder::class,
-            NecesidadSeeder::class,
-            FaltasSeeder::class,
-            TipoDonanteSeeder::class,
-            DonanteSeeder::class,
-            EncuestasSeeder::class,
-            PermissionSeeder::class,
-            RoleSeeder::class,
             UserSeeder::class,
-            // -----------------------------------------
-
+            PermissionSeeder::class,
         ]);
     }
 }
