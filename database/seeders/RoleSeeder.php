bc72b39faaf7a241f86bf4191c554e43ed647888
Marioo83GitHub <mcarbajalg@unah.hn>
2025-05-30 12:53:26 -0600

    cambios en seeders

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Exception;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
@@ -13,11 +15,30 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        $admin = Role::create(['name' => 'admin']);
-        // $admin = Role::find(1);
+        try {
+            $admin = Role::create(['name' => 'admin']);
+        }
+        catch (Exception $e)
+        {
+            $admin = Role::where('name', 'admin')->first();
+        }
 
         $permissions = Permission::all();
 
         $admin->syncPermissions($permissions);
+
+        $user = User::create([
+            'nombre' => 'Mario',
+            'apellido' => 'Carbajal',
+            'identidad' => '0601200303381',
+            'telefono' => '97639800',
+            'fecha_nacimiento' => '2003-09-02',
+            'estado_civil' => 'Soltero',
+            'email' => 'mcarbajalg@unah.hn',
+            'email_verified_at' => now(),
+            'password' => 'mcarbajalg123',
+        ]);
+
+        $user->assignRole($admin);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,54 +11,7 @@ class UserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        // Buscar usuarios por algún criterio (ej: email)
-        $user = User::where('email', 'ibaquedano@unah.hn')->first();
-        if ($user) {
-            $user->password = Hash::make('ibaquedano123');
-            $user->save();
-        }
-        dump($user->email);
+    public function run(): void {
 
-        // Buscar usuarios por algún criterio (ej: email)
-        $user = User::where('email', 'fsbetancourth@unah.hn')->first();
-        if ($user) {
-            $user->password = Hash::make('fsbetancourth123');
-            $user->save();
-        }
-        dump($user->email);
-
-        // Buscar usuarios por algún criterio (ej: email)
-        $user = User::where('email', 'dacia.espinoza@unah.hn')->first();
-        if ($user) {
-            $user->password = Hash::make('dacia.espinoza123');
-            $user->save();
-        }
-        dump($user->email);
-
-        // Buscar usuarios por algún criterio (ej: email)
-        $user = User::where('email', 'jorlin.rosa@unah.hn')->first();
-        if ($user) {
-            $user->password = Hash::make('jorlin.rosa123');
-            $user->save();
-        }
-        dump($user->email);
-
-        // Buscar usuarios por algún criterio (ej: email)
-        $user = User::where('email', 'cavila@unah.hn')->first();
-        if ($user) {
-            $user->password = Hash::make('cavila123');
-            $user->save();
-        }
-        dump($user->email);
-
-        // Buscar usuarios por algún criterio (ej: email)
-        $user = User::where('email', 'mcarbajalg@unah.hn')->first();
-        if ($user) {
-            $user->password = Hash::make('mcarbajalg123');
-            $user->save();
-        }
-        dump($user->email);
     }
 }
